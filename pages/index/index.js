dd2aa7233fc8c76a60acea09ca7361dfb2985d59
unknown <unknown@example.com>
2019-03-11 15:08:47 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,46 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    imgUrls: [
+      '/images/swiper01.jpg',
+      '/images/swiper02.jpg',
+      '/images/swiper03.jpg',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    proList:[
+      {
+        title: '精英贷',
+        shortDesc:'22周岁以上即可\n最快3小时下款\n件均8万,最高20万',
+        tell:'18926569267',
+        img:'/images/pro_01.jpg',
+        banner:'images/banner_01.jpg',
+        introduce:'1.打卡工资2000以上，或者社保公积金2000以上\n' + '2.22-60周岁\n'
+        + '带工作证明来公司就可以,最快3小时下款',
+      },
+      {
+        title: '月供贷',
+        shortDesc: '22周岁以上即可\n最快3小时下款\n件均8万,最高20万',
+        tell: '18926569267',
+        img: '/images/pro_02.jpg',
+        banner: 'images/banner_02.jpg',
+        introduce: '1.打卡工资2000以上，或者社保公积金2000以上\n' + '2.22-60周岁\n'
+          + '带工作证明来公司就可以,最快3小时下款',
+      },
+      {
+        title: '保单贷',
+        shortDesc: '22周岁以上即可\n最快3小时下款\n件均8万,最高20万',
+        tell: '18926569267',
+        img: '/images/pro_03.jpg',
+        banner: 'images/banner_03.jpg',
+        introduce: '1.打卡工资2000以上，或者社保公积金2000以上\n' + '2.22-60周岁\n'
+          + '带工作证明来公司就可以,最快3小时下款',
+      }
+    ]
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  
   }
 })
